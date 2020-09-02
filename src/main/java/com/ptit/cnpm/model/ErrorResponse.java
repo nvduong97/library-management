4fdef1af3eb707aad98bef47309d91fcc8938f2b
nvduong8 <duong97ptit@gmail.com>
2020-09-02 13:41:33 +0700

    fix bug

---
+++ src/main/java/com/ptit/cnpm/model/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.ptit.cnpm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ErrorResponse {
+    private HttpStatus status;
+    private String message;
+}
